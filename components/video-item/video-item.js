238d7278beed3d2ec5a207ba105def098f1c30f0
wffchong <283953455@qq.com>
2022-09-25 22:30:00 +0800

    渲染视频列表

---
+++ components/video-item/video-item.js
@@ -0,0 +1,12 @@
+// components/video-item/video-item.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        itemData: {
+            type: Object,
+            value: {}
+        }
+    }
+})
