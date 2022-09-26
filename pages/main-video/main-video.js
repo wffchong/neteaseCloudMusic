d09a364f4e59c528d51415000890118fc254f166
wffchong <283953455@qq.com>
2022-09-26 09:24:02 +0800

    视频列表下拉刷新和触底加载

--- pages/main-video/main-video.js
+++ pages/main-video/main-video.js
@@ -6,7 +6,9 @@ import {
 
 Page({
     data: {
-        videoList: []
+        videoList: [],
+        offset: 0,
+        hasMore: true
     },
     onLoad() {
         this.fetchTopMv()
@@ -14,10 +16,37 @@ Page({
 
     async fetchTopMv() {
         const {
-            data
-        } = await getTopMv()
+            data,
+            hasMore
+        } = await getTopMv(this.data.offset)
+        const videoListRes = [...this.data.videoList, ...data]
         this.setData({
-            videoList: data
+            hasMore,
+            offset: videoListRes.length,
+            videoList: videoListRes
         })
+    },
+
+    // 触底加载
+    onReachBottom() {
+        // 判断是否有更多数据
+        if (!this.data.hasMore) return
+
+        this.fetchTopMv(this.data.offset)
+    },
+
+    // 下拉刷新
+    async onPullDownRefresh() {
+        this.setData({
+            offset: 0,
+            videoList: [],
+            hasMore: true
+        })
+
+        // 重新请求数据
+        await this.fetchTopMv()
+
+        // 停止下拉刷新
+        wx.stopPullDownRefresh()
     }
 })
